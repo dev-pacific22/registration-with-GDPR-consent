0cc95dad07839367d1be33a98a2673c1f0e2b99c
Prashant <dev.pacific22@gmail.com>
2018-08-18 00:13:03 +0530

    new: Added redux library for State management in app.

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 /**
- * Sample React Native App
+ * 
+ * React Native App for Registration Process with GDPR consents
  * https://github.com/facebook/react-native
- *
- * @format
- * @flow
  */
 
 import React, { Component } from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
+import reducersAll  from "./src/reducers";
 import { NavigationStack } from "./src/routes/Navigation";
 
 export default class App extends Component {
   render() {
-    return <NavigationStack />;
+    const store = createStore(reducersAll,  applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <NavigationStack />
+      </Provider>
+    );
   }
 }
-
--- src/container/RegistrationScreen.js
+++ src/container/RegistrationScreen.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import { View, Text, Button } from "native-base";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { onRegisterClickAction } from "../actions";
 
 class RegistrationScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
+  onRegisterClick = message => {
+    this.props.onRegisterClickAction(message);
+  };
   render() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          marginTop: 50,
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <Text> {this.props.message} </Text>
+        <Button onPress={() => this.onRegisterClick("Test")}>
           <Text>Home Screen</Text>
         </Button>
       </View>
@@ -22,4 +32,16 @@ class RegistrationScreen extends Component {
   }
 }
 
-export default RegistrationScreen;
+const mapStateToProps = ({ registration }) => {
+  const { email, password, error, loading, message } = registration;
+  return { email, password, error, loading, message };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ onRegisterClickAction }, dispatch);
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RegistrationScreen);
